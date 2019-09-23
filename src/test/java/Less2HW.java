07afdda259e86947078a384e91ff5788560c8ba8
Denys Kozachenko <dkozachenko2@gmal.com>
2019-09-23 12:55:38 +0300

    Initial commit

---
+++ src/test/java/Less2HW.java
@@ -0,0 +1,28 @@
+public class Less2HW {
+    public static void main(String[] args) {
+
+        String a = "Hello world";
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+
+        byte c = 120;
+        short k = 15;
+        long m = 4l;
+        boolean s = false;
+        int l = 8;
+        float b = 2.14f;
+        double d = 2.11d;
+        char g = 'm';
+        System.out.println(c +" "+ k );
+        System.out.println(culculateSquare(2,3));
+    }
+    public static int  culculateSquare (int a1, int b1){
+        int result = a1 * b1;
+
+        return result;
+    }
+
+}
